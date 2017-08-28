b0bc87186d1b0c22f210c370f1c48a41fa6d1d98
Tom Cox <tcox2603@gmail.com>
2017-08-28 11:13:05 +1000

    Removed unnecessary constant

--- box_interface.py
+++ box_interface.py
@@ -12,9 +12,6 @@ from wsgiref.simple_server import WSGIServer, WSGIRequestHandler, make_server
 
 CONFIG_FILE = 'box_app.cfg'
 REQUEST_COUNT = 200
-QUERY_STRING = 'a b c d e f g h i j k l m n o p q r s t u v w y z '\
-               + 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z '\
-               + '0 1 2 3 4 5 6 7 8 9 . - _ ( ) # [ ] : / * + ='
 
 
 class StoppableWSGIServer(bottle.ServerAdapter):
